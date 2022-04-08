3a542f5528ebb697bfffd29f6a47d4c0f7369939
hir01 <bk0831666@gmail.com>
2022-04-08 21:22:44 +0300

    начало конца

--- src/js/main.js
+++ src/js/main.js
@@ -7,6 +7,8 @@ import {actualYear} from './modules/actualYear';
 import header from './components/header';
 import lazyLoading from './modules/lazyLoading';
 import scrollToAnchor from './modules/scrollToAnchor';
+// import Preloader from 'ninelines-preloader';
+// import LocomotiveScroll from 'locomotive-scroll';
 
 ieFix();
 vhFix();
@@ -15,3 +17,9 @@ scrollToAnchor.init();
 
 header.init();
 lazyLoading.init();
+
+// Preloader.init('', 0)
+// 	.then(() => {
+// 		document.querySelector('.loader').style.display = 'none';
+// 		// showPage();
+// 	});
